220a039a4699228c934106006bf00400810c5581
wujiazhong <wujz@ADMINIB-31C38UD.spss.com>
2015-08-06 14:19:14 +0800

    add new file

---
+++ src/servlet/GetWellOperDate_do.java
@@ -0,0 +1,89 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DAO;
+import net.sf.json.JSONException;
+/*import net.sf.json.JSONObject;*/
+import net.sf.json.JSONArray;
+
+public class GetWellOperDate_do extends HttpServlet {
+	private static final long serialVersionUID = 1L;  
+	/**
+	 * Constructor of the object.
+	 */
+	public GetWellOperDate_do() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8"); 
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		final String userteam = request.getParameter("userteam");
+
+		DAO t_userteam_well_query = new DAO();
+		JSONArray oper_date = t_userteam_well_query.getOperDateList(userteam);
+		
+		try {
+			PrintWriter out = response.getWriter();
+			out.write(oper_date.toString());
+			out.flush();
+			out.close();
+			out = null;
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
